3bd73cfd4ead19a8e432e481012c4564fee14998
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-02-04 18:24:05 +0200

    Add unit tests for ChannelController/Get all, using both NUnit and xUnit

---
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.NUnit.Test/Controllers/ChannelControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using CSharp.UnitTesting.Api.Controllers;
+using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CSharp.UnitTesting.Api.NUnit.Test.Controllers
+{
+    [Property("NUnit", "Channel Controller")]
+    public class ChannelControllerTest
+    {
+        private Mock<IChannelService> mockChannelService;
+        private ChannelController channelController;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockChannelService = new Mock<IChannelService>();
+            channelController = new ChannelController(mockChannelService.Object);
+        }
+
+        [Test]
+        [Property("HttpVerb", "GET")]
+        public async Task GivenGetAllAsyncWhenDataExistThenReturnsData()
+        {
+            // Arrange
+            mockChannelService
+                .Setup(_ => _.GetAllAsync())
+                .ReturnsAsync(It.IsAny<IEnumerable<Channel>>())
+                .Verifiable();
+
+            // Act
+            var response = await channelController.GetAllAsync();
+
+            // Assert
+            mockChannelService.VerifyAll();
+            Assert.That(response, Is.InstanceOf<ObjectResult>());
+            var result = response as OkObjectResult;
+            Assert.That(result.StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+        }
+
+        [Test]
+        [Property("HttpVerb", "GET")]
+        public async Task GivenGetAllAsyncWhenExceptionThrownThenHandlesGracefully()
+        {
+            // Arrange
+            mockChannelService
+                .Setup(_ => _.GetAllAsync())
+                .Throws<Exception>()
+                .Verifiable();
+
+            // Act
+            var response = await channelController.GetAllAsync();
+
+            // Assert
+            mockChannelService.VerifyAll();
+            Assert.That(response, Is.InstanceOf<BadRequestResult>());
+            var result = response as BadRequestResult;
+            Assert.That(result.StatusCode, Is.EqualTo((int)HttpStatusCode.BadRequest));
+        }
+    }
+}
---
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.Xunit.Test/Controllers/ChannelControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using CSharp.UnitTesting.Api.Controllers;
+using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharp.UnitTesting.Api.Xunit.Test.Controllers
+{
+    [Trait("xUnit", "Channel Controller")]
+    public class ChannelControllerTest
+    {
+        private readonly Mock<IChannelService> mockChannelService;
+        private readonly ChannelController channelController;
+
+        public ChannelControllerTest()
+        {
+            mockChannelService = new Mock<IChannelService>();
+            channelController = new ChannelController(mockChannelService.Object);
+        }
+
+        [Fact]
+        [Trait("HttpVerb", "GET")]
+        internal async Task GivenGetAllAsyncWhenDataExistThenReturnsData()
+        {
+            // Arrange
+            mockChannelService
+                .Setup(_ => _.GetAllAsync())
+                .ReturnsAsync(It.IsAny<IEnumerable<Channel>>())
+                .Verifiable();
+
+            // Act
+            var response = await channelController.GetAllAsync();
+
+            // Assert
+            mockChannelService.VerifyAll();
+            var result = Assert.IsType<OkObjectResult>(response);
+            Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Fact]
+        [Trait("HttpVerb", "GET")]
+        internal async Task GivenGetAllAsyncWhenExceptionThrownThenHandlesGracefully()
+        {
+            // Arrange
+            mockChannelService
+                .Setup(_ => _.GetAllAsync())
+                .Throws<Exception>()
+                .Verifiable();
+
+            // Act
+            var response = await channelController.GetAllAsync();
+
+            // Assert
+            mockChannelService.VerifyAll();
+            var result = Assert.IsType<BadRequestResult>(response);
+            Assert.Equal((int)HttpStatusCode.BadRequest, result.StatusCode);
+        }
+    }
+}
