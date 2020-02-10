cc94789fd33035c05c766d786e5dd845c0008ee4
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-02-10 16:18:00 +0200

    NUnit - Add unit tests for all services

---
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.NUnit.Test/Services/PlaylistServiceTest.cs
@@ -0,0 +1,148 @@
+﻿using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Services;
+using CSharp.UnitTesting.Api.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharp.UnitTesting.Api.NUnit.Test.Services
+{
+    [Property("NUnit", "Service | Playlist")]
+    public class PlaylistServiceTest
+    {
+        private readonly IPlaylistService playlistService;
+
+        public PlaylistServiceTest()
+        {
+            playlistService = new PlaylistService();
+        }
+
+        [Test]
+        public void GivenGetAllAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.GetAllAsync();
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenGetByIdAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.GetByIdAsync(It.IsAny<Guid>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenGetByIdsAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.GetByIdsAsync(It.IsAny<IEnumerable<Guid>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenCreateAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.CreateAsync(It.IsAny<Playlist>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenCreateBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.CreateBulkAsync(It.IsAny<IEnumerable<Playlist>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenUpdateAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.UpdateAsync(It.IsAny<Playlist>(), It.IsAny<Guid>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenUpdateBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.UpdateBulkAsync(It.IsAny<IEnumerable<Playlist>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenDeleteAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.DeleteAsync(It.IsAny<Guid>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenDeleteBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await playlistService.DeleteBulkAsync(It.IsAny<IEnumerable<Guid>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+    }
+}
---
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.NUnit.Test/Services/SubscriptionServiceTest.cs
@@ -0,0 +1,162 @@
+﻿using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Services;
+using CSharp.UnitTesting.Api.Services.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharp.UnitTesting.Api.NUnit.Test.Services
+{
+    [Property("NUnit", "Service | Subscription")]
+    public class SubscriptionServiceTest
+    {
+        private readonly ISubscriptionService subscriptionService;
+
+        public SubscriptionServiceTest()
+        {
+            subscriptionService = new SubscriptionService();
+        }
+
+        [Test]
+        public void GivenGetAllAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.GetAllAsync();
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenGetByIdAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.GetByIdAsync(It.IsAny<Guid>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenGetByIdsAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.GetByIdsAsync(It.IsAny<IEnumerable<Guid>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenGetByChannelIdAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.GetByChannelIdAsync(It.IsAny<int>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenCreateAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.CreateAsync(It.IsAny<Subscription>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenCreateBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.CreateBulkAsync(It.IsAny<IEnumerable<Subscription>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenUpdateAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.UpdateAsync(It.IsAny<Subscription>(), It.IsAny<Guid>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenUpdateBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.UpdateBulkAsync(It.IsAny<IEnumerable<Subscription>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenDeleteAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.DeleteAsync(It.IsAny<Guid>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+
+        [Test]
+        public void GivenDeleteBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await subscriptionService.DeleteBulkAsync(It.IsAny<IEnumerable<Guid>>());
+
+            // Act
+            var exception = Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception, Is.TypeOf<NotImplementedException>());
+        }
+    }
+}
