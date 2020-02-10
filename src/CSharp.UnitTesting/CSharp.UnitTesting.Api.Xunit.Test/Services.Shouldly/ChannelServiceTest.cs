8ce7ae681d6695ad2d6e5478334c8d5d2fff2651
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-02-10 15:28:58 +0200

    xUnit - Add unit tests for all services, with Shouldly

---
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.Xunit.Test/Services.Shouldly/ChannelServiceTest.cs
@@ -0,0 +1,163 @@
+﻿using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Services;
+using CSharp.UnitTesting.Api.Services.Interfaces;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharp.UnitTesting.Api.Xunit.Test.Services.Shouldly
+{
+    [Trait("xUnit | Shouldly", "Service | Channel")]
+    public class ChannelServiceTest
+    {
+        private readonly IChannelService channelService;
+
+        public ChannelServiceTest()
+        {
+            channelService = new ChannelService();
+        }
+
+        [Fact]
+        internal async Task GivenGetAllAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.GetAllAsync();
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenGetByIdAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.GetByIdAsync(It.IsAny<int>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenGetByIdsAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.GetByIdsAsync(It.IsAny<IEnumerable<int>>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenGetTopAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.GetTopAsync(It.IsAny<int>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenCreateAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.CreateAsync(It.IsAny<Channel>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenCreateBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.CreateBulkAsync(It.IsAny<IEnumerable<Channel>>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenUpdateAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.UpdateAsync(It.IsAny<Channel>(), It.IsAny<int>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenUpdateBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.UpdateBulkAsync(It.IsAny<IEnumerable<Channel>>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenDeleteAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.DeleteAsync(It.IsAny<int>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+
+        [Fact]
+        internal async Task GivenDeleteBulkAsyncWhenExpectedExceptionIsThrownThenHandlesGracefully()
+        {
+            // Arrange
+            async Task action() => await channelService.DeleteBulkAsync(It.IsAny<IEnumerable<int>>());
+
+            // Act
+            var exception = await Assert.ThrowsAsync<NotImplementedException>(action);
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<NotImplementedException>();
+        }
+    }
+}
