c56a374d25fcd5797371c4c1428347a4ef38bf83
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-05 13:57:14 +0300

    Improve unit tests and assertions for Entities on NUnit with default assertions

--- src/CSharp.UnitTesting/CSharp.UnitTesting.Api.NUnit.Test/Data/Entities/PlaylistTest.cs
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.NUnit.Test/Data/Entities/PlaylistTest.cs
@@ -1,4 +1,5 @@
 ﻿using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Data.Entities.Enums;
 using CSharp.UnitTesting.Api.Utils.DataFaker;
 using CSharp.UnitTesting.Api.Utils.DataFaker.Interfaces;
 using NUnit.Framework;
@@ -7,8 +8,8 @@ using System.Collections.Generic;
 
 namespace CSharp.UnitTesting.Api.NUnit.Test.Data.Entities
 {
-    [Property("NUnit", "Entity | Playlist")]
-    public class PlaylistTest
+    [Property("NUnit + Default | Data | Entities", nameof(Playlist))]
+    public sealed class PlaylistTest
     {
         [Test]
         public void GivenPlaylistEntityWhenGeneratedWithDataFakerThenVerifyAllProperties()
@@ -17,7 +18,7 @@ namespace CSharp.UnitTesting.Api.NUnit.Test.Data.Entities
             IDataFaker dataFaker = new DataFaker();
 
             // Act
-            var playlists = dataFaker.FakePlaylist.Generate(count: 99);
+            var playlists = dataFaker.FakePlaylist.Generate(count: 10);
 
             // Assert
             playlists.ForEach(playlist =>
@@ -31,12 +32,16 @@ namespace CSharp.UnitTesting.Api.NUnit.Test.Data.Entities
                 Assert.That(playlist.Description, Is.Not.Empty);
                 Assert.That(playlist.Description.Length, Is.InRange(1, 100));
                 Assert.That(playlist.AccessType, Is.Not.Null);
+                Assert.That(playlist.AccessType, Is.TypeOf<PlaylistAccessType>());
                 Assert.That(playlist.CreatedAt, Is.Not.Null);
-                Assert.That(playlist.CreatedAt, Is.InRange(DateTime.UtcNow.AddDays(-365), DateTime.UtcNow));
+                Assert.That(playlist.CreatedAt, Is.GreaterThanOrEqualTo(DateTime.UtcNow.AddDays(-365)));
+                Assert.That(playlist.CreatedAt, Is.LessThanOrEqualTo(DateTime.UtcNow));
                 Assert.That(playlist.Videos, Is.Not.Null);
-                Assert.That(playlist.Videos, Is.Not.Empty);
                 Assert.That(playlist.Videos, Is.InstanceOf<IEnumerable<Video>>());
+                Assert.That(playlist.Videos, Is.Not.Empty);
+                Assert.That(playlist.Videos, Has.Count.EqualTo(3));
                 Assert.That(playlist.IsDeleted, Is.Not.Null);
+                Assert.That(playlist.IsDeleted, Is.TypeOf<bool>());
             });
         }
     }
