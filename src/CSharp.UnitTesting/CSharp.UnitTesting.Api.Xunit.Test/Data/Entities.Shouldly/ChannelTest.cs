9288f1571ac8dce63e76c6684dade66be942171b
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-05 15:40:04 +0300

    Improve unit tests and assertions for Entities on xUnit with Shouldly

--- src/CSharp.UnitTesting/CSharp.UnitTesting.Api.Xunit.Test/Data/Entities.Shouldly/ChannelTest.cs
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.Xunit.Test/Data/Entities.Shouldly/ChannelTest.cs
@@ -3,12 +3,13 @@ using CSharp.UnitTesting.Api.Utils.DataFaker;
 using CSharp.UnitTesting.Api.Utils.DataFaker.Interfaces;
 using Shouldly;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace CSharp.UnitTesting.Api.Xunit.Test.Data.Entities.Shouldly
 {
-    [Trait("xUnit | Shouldly", "Entity | Channel")]
-    public class ChannelTest
+    [Trait("xUnit + Shouldly | Data | Entities", nameof(Channel))]
+    public sealed class ChannelTest
     {
         [Fact]
         internal void GivenChannelEntityWhenGeneratedWithDataFakerThenVerifyAllProperties()
@@ -17,28 +18,31 @@ namespace CSharp.UnitTesting.Api.Xunit.Test.Data.Entities.Shouldly
             IDataFaker dataFaker = new DataFaker();
 
             // Act
-            var channels = dataFaker.FakeChannel.Generate(count: 99);
+            var channels = dataFaker.FakeChannel.Generate(count: 10);
 
             // Assert
             channels.ForEach(
                 channel => channel.ShouldSatisfyAllConditions(
                     () => channel.Id.ShouldNotBeNull(),
-                    () => channel.Id.ShouldBeGreaterThanOrEqualTo(1, "because it is PK"),
-                    () => channel.Name.ShouldNotBeNullOrEmpty("because it is required"),
+                    () => channel.Id.ShouldBeGreaterThanOrEqualTo(1),
+                    () => channel.Name.ShouldNotBeNullOrEmpty(),
                     () => channel.Name.Length.ShouldBeInRange(1, 50),
-                    () => channel.Description.ShouldNotBeNullOrEmpty("because it is required"),
+                    () => channel.Description.ShouldNotBeNullOrEmpty(),
                     () => channel.Description.Length.ShouldBeInRange(1, 100),
-                    () => channel.Avatar.ShouldNotBeNull("because it is required"),
+                    () => channel.Avatar.ShouldNotBeNull(),
                     () => channel.Avatar.ShouldNotBeEmpty(),
                     () => channel.OwnerEmail.ShouldNotBeNullOrEmpty(),
                     () => channel.OwnerEmail.ShouldContain('@'),
-                    () => channel.Subscriptions.ShouldNotBeNull("because it is required"),
-                    () => channel.Subscriptions.ShouldNotBeEmpty(),
+                    () => channel.Subscriptions.ShouldNotBeNull(),
                     () => channel.Subscriptions.ShouldBeAssignableTo<IEnumerable<Subscription>>(),
+                    () => channel.Subscriptions.ShouldNotBeEmpty(),
+                    () => channel.Subscriptions.Count().ShouldBe(3),
                     () => channel.Videos.ShouldNotBeNull(),
-                    () => channel.Videos.ShouldNotBeEmpty(),
                     () => channel.Videos.ShouldBeAssignableTo<IEnumerable<Video>>(),
-                    () => channel.IsDeleted.ShouldNotBeNull("because it is required")));
+                    () => channel.Videos.ShouldNotBeEmpty(),
+                    () => channel.Videos.Count().ShouldBe(3),
+                    () => channel.IsDeleted.ShouldNotBeNull(),
+                    () => channel.IsDeleted.ShouldBeOneOf(true, false)));
         }
     }
 }
--- src/CSharp.UnitTesting/CSharp.UnitTesting.Api.Xunit.Test/Data/Entities.Shouldly/PlaylistTest.cs
+++ src/CSharp.UnitTesting/CSharp.UnitTesting.Api.Xunit.Test/Data/Entities.Shouldly/PlaylistTest.cs
@@ -1,15 +1,17 @@
 ﻿using CSharp.UnitTesting.Api.Data.Entities;
+using CSharp.UnitTesting.Api.Data.Entities.Enums;
 using CSharp.UnitTesting.Api.Utils.DataFaker;
 using CSharp.UnitTesting.Api.Utils.DataFaker.Interfaces;
 using Shouldly;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace CSharp.UnitTesting.Api.Xunit.Test.Data.Entities.Shouldly
 {
-    [Trait("xUnit | Shouldly", "Entity | Playlist")]
-    public class PlaylistTest
+    [Trait("xUnit + Shouldly | Data | Entities", nameof(Playlist))]
+    public sealed class PlaylistTest
     {
         [Fact]
         internal void GivenPlaylistEntityWhenGeneratedWithDataFakerThenVerifyAllProperties()
@@ -18,24 +20,28 @@ namespace CSharp.UnitTesting.Api.Xunit.Test.Data.Entities.Shouldly
             IDataFaker dataFaker = new DataFaker();
 
             // Act
-            var playlists = dataFaker.FakePlaylist.Generate(count: 99);
+            var playlists = dataFaker.FakePlaylist.Generate(count: 10);
 
             // Assert
             playlists.ForEach(
                 playlist => playlist.ShouldSatisfyAllConditions(
                     () => playlist.Id.ShouldNotBeNull(),
-                    () => playlist.Id.ShouldNotBe(Guid.Empty, "because it is PK"),
-                    () => playlist.Name.ShouldNotBeNullOrEmpty("because it is required"),
+                    () => playlist.Id.ShouldNotBe(Guid.Empty),
+                    () => playlist.Name.ShouldNotBeNullOrEmpty(),
                     () => playlist.Name.Length.ShouldBeInRange(1, 50),
-                    () => playlist.Description.ShouldNotBeNullOrEmpty("because it is required"),
+                    () => playlist.Description.ShouldNotBeNullOrEmpty(),
                     () => playlist.Description.Length.ShouldBeInRange(1, 100),
-                    () => playlist.AccessType.ShouldNotBeNull("because it is required"),
+                    () => playlist.AccessType.ShouldNotBeNull(),
+                    () => playlist.AccessType.ShouldBeOfType<PlaylistAccessType>(),
+                    () => playlist.CreatedAt.ShouldNotBeNull(),
                     () => playlist.CreatedAt.ShouldBeGreaterThanOrEqualTo(DateTime.UtcNow.AddDays(-365)),
                     () => playlist.CreatedAt.ShouldBeLessThanOrEqualTo(DateTime.UtcNow),
                     () => playlist.Videos.ShouldNotBeNull(),
                     () => playlist.Videos.ShouldNotBeEmpty(),
                     () => playlist.Videos.ShouldBeAssignableTo<IEnumerable<Video>>(),
-                    () => playlist.IsDeleted.ShouldNotBeNull("because it is required")));
+                    () => playlist.Videos.Count().ShouldBe(3),
+                    () => playlist.IsDeleted.ShouldNotBeNull(),
+                    () => playlist.IsDeleted.ShouldBeOneOf(true, false)));
         }
     }
 }
